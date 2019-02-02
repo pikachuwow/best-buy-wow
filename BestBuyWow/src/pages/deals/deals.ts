5831a765a1b1011b3276d0a965896f1a4799f011
royhwang12 <royhwang22@gmail.com>
2019-02-02 06:00:33 -0800

    changes

--- BestBuyWow/src/pages/deals/deals.ts
+++ BestBuyWow/src/pages/deals/deals.ts
@@ -4,7 +4,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 type IDeal = {
   name: string,
   description: string,
-  price: string
+  price: string,
+  url: string
 }
 @IonicPage()
 @Component({
@@ -17,84 +18,82 @@ export class DealsPage {
     {
       name: 'Amazon Alexa',
       description: 'Amazon smart home device',
-      price: '$50.00'
+      price: '$50.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/amazon-echo-plus-2nd-generation-with-alexa-english-charcoal/13023974.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Google Home',
+      description: 'Smart Home Device from Google',
+      price: '$70.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/google-home-white-slate/10721100.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'MSI GS65 Gaming Laptop',
+      description: 'The ultimate gaming laptopl',
+      price: '$10 000.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/msi-gs65-stealth-thin-15-6-gaming-laptop-16gb-256gb-intel-core-i7-8750h-geforce-gtx1070-win-10-pro-gs65-8rf-074ca-stealth/12618562.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Apple Macbook Pro',
+      description: 'Simplicity is the ultimate sophistication',
+      price: '$3000.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/apple-macbook-pro-with-touch-bar-15-4-space-grey-intel-core-i7-2-6ghz-512gb-16gb-ram-english/12727802.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Ipad Pro',
+      description: 'Power of ',
+      price: '$500.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/apple-ipad-pro-12-9-512gb-with-wi-fi-4g-lte-space-grey/10750291.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Bose QC35 II Headphones',
+      description: 'Revolutionary noise cancelling headphones',
+      price: '$450.00',
+      url: ''
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Samsung Ultrawide Gaming Monitor',
+      description: 'High refresh rate and greater field of view',
+      price: '$1000.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/samsung-49-ultrawide-144hz-1ms-curved-led-gaming-monitor-lc49hg90dmnxza-charcoal-black/11292475.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Canon Laser Printer',
+      description: 'Ultra-fast printing speeds and extreme colour accuracy',
+      price: '$300.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/canon-imageclass-mf632cdw-colour-wireless-laser-printer/10748131.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Apple Watch Series 4',
+      description: 'Power of a smartphone on the wrist of your arm',
+      price: '$500.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/apple-watch-series-4-gps-44mm-space-grey-aluminium-case-with-black-sport-band/13018343.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Insignia Freezer',
+      description: 'Freeze your food items at rapid speeds',
+      price: '$139.99',
+      url: 'https://www.bestbuy.ca/en-ca/product/insignia-3-5-cu-ft-chest-freezer-ns-cz35wh9/12656187.aspx?icmp=Recos_3across_tp_sllng_prdcts'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Playstation 4 1TB',
+      description: 'Play games at high definition resolutions',
+      price: '$500.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/playstation-4-pro-1tb-console/12910052.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: 'Fender Stratocaster',
+      description: 'An electric guitar that suits all styles of music',
+      price: '$900.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/fender-player-stratocaster-electric-guitar-tidepool/13024761.aspx?'
     },
     {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
+      name: '2.4G 4-Channel Foldable Drone',
+      description: 'Has a 720p built-in camera and much user control and interation with the drone.',
+      price: '$100.00',
+      url: 'https://www.bestbuy.ca/en-ca/product/2-4g-4-channel-foldable-drone-with-wifi-720p-camera-altitude-hold-mode/12598527.aspx?'
     },
-    {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
-    },
-    {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
-    },
-    {
-      name: 'Amazon Alexa',
-      description: 'Amazon smart home device',
-      price: '$50.00'
-    },
-  ]
+    ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
